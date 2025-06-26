4dbb526ef77059b4c8ea9f1868d28a7b933e3352
shiyamsvk <42956481+shiyamsvk@users.noreply.github.com>
2025-06-26 15:40:41 +0530

    Added important methods

---
+++ JavaExamples/src/com/java/Collection/Demo/ArrayListAllMethodsDemo.java
@@ -0,0 +1,129 @@
+package com.java.Collection.Demo;
+
+import java.util.*;
+import java.util.function.*;
+
+public class ArrayListAllMethodsDemo {
+    public static void main(String[] args) {
+
+        // 1. Creating ArrayList
+        ArrayList<String> list = new ArrayList<>();
+
+        // 2. add(E e)
+        list.add("Apple");
+        list.add("Banana");
+
+        // 3. add(index, element)
+        list.add(1, "Cherry");
+
+        // 4. addAll(Collection)
+        ArrayList<String> newFruits = new ArrayList<>(Arrays.asList("Dates", "Elderberry"));
+        list.addAll(newFruits);
+
+        // 5. addAll(index, Collection)
+        list.addAll(2, Arrays.asList("Fig", "Grapes"));
+
+        // 6. contains(Object)
+        System.out.println("Contains Banana? " + list.contains("Banana"));
+
+        // 7. get(index)
+        System.out.println("Element at index 3: " + list.get(3));
+
+        // 8. indexOf(Object)
+        System.out.println("Index of Cherry: " + list.indexOf("Cherry"));
+
+        // 9. lastIndexOf(Object)
+        list.add("Apple");
+        System.out.println("Last index of Apple: " + list.lastIndexOf("Apple"));
+
+        // 10. isEmpty()
+        System.out.println("Is list empty? " + list.isEmpty());
+
+        // 11. iterator()
+        System.out.println("\nUsing Iterator:");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+
+        // 12. listIterator()
+        System.out.println("\n\nUsing ListIterator:");
+        ListIterator<String> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+
+        // 13. listIterator(index)
+        System.out.println("\n\nListIterator from index 2:");
+        listIterator = list.listIterator(2);
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+
+        // 14. remove(index)
+        System.out.println("\n\nRemoved element at index 2: " + list.remove(2));
+
+        // 15. remove(Object)
+        list.remove("Apple");
+
+        // 16. removeAll(Collection)
+        list.removeAll(Arrays.asList("Banana", "Grapes"));
+
+        // 17. removeIf(Predicate)
+        list.removeIf(s -> s.startsWith("E"));
+
+        // 18. retainAll(Collection)
+        list.retainAll(Arrays.asList("Cherry", "Dates"));
+
+        // 19. set(index, element)
+        list.set(0, "Coconut");
+
+        // 20. size()
+        System.out.println("\nSize of list: " + list.size());
+
+        // 21. forEach
+        System.out.println("\nUsing forEach:");
+        list.forEach(System.out::println);
+
+        // 22. toArray()
+        Object[] array = list.toArray();
+        System.out.println("\nArray: " + Arrays.toString(array));
+
+        // 23. toArray(T[] a)
+        String[] strArray = list.toArray(new String[0]);
+        System.out.println("Typed Array: " + Arrays.toString(strArray));
+
+        // 24. subList(fromIndex, toIndex)
+        list.add("Banana");
+        list.add("Dragonfruit");
+        List<String> subList = list.subList(0, 2);
+        System.out.println("SubList: " + subList);
+
+        // 25. sort(Comparator)
+        list.sort(Comparator.naturalOrder());
+        System.out.println("Sorted list: " + list);
+
+        // 26. replaceAll(UnaryOperator)
+        list.replaceAll(String::toUpperCase);
+        System.out.println("Replaced all to uppercase: " + list);
+
+        // 27. clone()
+        ArrayList<String> clonedList = (ArrayList<String>) list.clone();
+        System.out.println("Cloned List: " + clonedList);
+
+        // 28. ensureCapacity()
+        list.ensureCapacity(20);
+
+        // 29. trimToSize()
+        list.trimToSize();
+
+        // 30. spliterator()
+        System.out.println("\nUsing Spliterator:");
+        Spliterator<String> spliterator = list.spliterator();
+        spliterator.forEachRemaining(System.out::println);
+
+        // 31. clear()
+        list.clear();
+        System.out.println("\nAfter clear, is list empty? " + list.isEmpty());
+    }
+}
---
+++ JavaExamples/src/com/java/Collection/Demo/LinkedListAllMethodsDemo.java
@@ -0,0 +1,117 @@
+package com.java.Collection.Demo;
+
+import java.util.*;
+
+public class LinkedListAllMethodsDemo {
+    public static void main(String[] args) {
+
+        // Create LinkedList
+        LinkedList<String> list = new LinkedList<>();
+
+        // add(E e), add(int index, E element)
+        list.add("Banana");
+        list.add("Apple");
+        list.add(1, "Mango");
+
+        // addFirst(E e), addLast(E e)
+        list.addFirst("Orange");
+        list.addLast("Pineapple");
+
+        // offer(E e), offerFirst(E e), offerLast(E e)
+        list.offer("Guava");
+        list.offerFirst("Kiwi");
+        list.offerLast("Papaya");
+
+        // peek(), peekFirst(), peekLast()
+        System.out.println("peek(): " + list.peek());
+        System.out.println("peekFirst(): " + list.peekFirst());
+        System.out.println("peekLast(): " + list.peekLast());
+
+        // poll(), pollFirst(), pollLast()
+        System.out.println("poll(): " + list.poll());
+        System.out.println("pollFirst(): " + list.pollFirst());
+        System.out.println("pollLast(): " + list.pollLast());
+
+        // push(E e), pop()
+        list.push("Strawberry");
+        System.out.println("After push: " + list);
+        System.out.println("Pop: " + list.pop());
+
+        // get(index), getFirst(), getLast()
+        System.out.println("get(2): " + list.get(2));
+        System.out.println("getFirst(): " + list.getFirst());
+        System.out.println("getLast(): " + list.getLast());
+
+        // contains(Object o), indexOf(), lastIndexOf()
+        System.out.println("Contains Mango? " + list.contains("Mango"));
+        System.out.println("Index of Apple: " + list.indexOf("Apple"));
+        list.add("Apple");
+        System.out.println("Last index of Apple: " + list.lastIndexOf("Apple"));
+
+        // remove(), remove(index), remove(Object), removeFirst(), removeLast()
+        list.remove();  // head
+        list.remove(1);
+        list.remove("Mango");
+        list.removeFirst();
+        list.removeLast();
+
+        // removeFirstOccurrence, removeLastOccurrence
+        list.removeFirstOccurrence("Apple");
+        list.removeLastOccurrence("Apple");
+
+        // addAll(Collection)
+        List<String> moreFruits = Arrays.asList("Fig", "Grapes", "Watermelon");
+        list.addAll(moreFruits);
+
+        // addAll(index, Collection)
+        list.addAll(1, Arrays.asList("Dates", "Blueberry"));
+
+        // listIterator(index)
+        System.out.println("\nList from index 2:");
+        ListIterator<String> listIterator = list.listIterator(2);
+        while (listIterator.hasNext()) {
+            System.out.print(listIterator.next() + " ");
+        }
+
+        // descendingIterator()
+        System.out.println("\n\nDescending order:");
+        Iterator<String> descending = list.descendingIterator();
+        while (descending.hasNext()) {
+            System.out.print(descending.next() + " ");
+        }
+
+        // set(index, E element)
+        list.set(0, "Jackfruit");
+
+        // size()
+        System.out.println("\n\nSize: " + list.size());
+
+        // clone()
+        LinkedList<String> clonedList = (LinkedList<String>) list.clone();
+        System.out.println("Cloned List: " + clonedList);
+
+        // removeIf
+        list.removeIf(s -> s.startsWith("G"));
+
+        // toArray()
+        Object[] array = list.toArray();
+        System.out.println("toArray(): " + Arrays.toString(array));
+
+        // toArray(T[] a)
+        String[] strArray = list.toArray(new String[0]);
+        System.out.println("toArray(T[]): " + Arrays.toString(strArray));
+
+        // spliterator
+        System.out.println("\nUsing Spliterator:");
+        Spliterator<String> spliterator = list.spliterator();
+        spliterator.forEachRemaining(System.out::println);
+
+        // forEach
+        System.out.println("\nUsing forEach:");
+        list.forEach(System.out::println);
+
+        // clear()
+        list.clear();
+        System.out.println("\nIs list empty after clear? " + list.isEmpty());
+    }
+}
